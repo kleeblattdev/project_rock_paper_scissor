677964aa09e5468fdba59e2f1c3d90141e9d641e
karinlee <karinlee92@gmail.com>
2023-02-02 12:18:51 +0100

    added rock,paper, scissor functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 //Variables
 
 // HTML Sections or text elements
-const rounds = document.querySelector("#rounds");
+const roundsSelection = document.querySelector("#roundsSelection");
 const roundsCounter = document.querySelector("#roundsCounter");
 const roundOf = document.querySelector("#roundOf");
 
@@ -21,12 +21,65 @@ const playerPoints = document.querySelector("#playerPoints");
 const cpuPoints = document.querySelector("#cpuPoints");
 const resultText = document.querySelector("#resultText");
 
-console.log(rounds, roundsCounter, roundOf);
-
-console.log(roundFive, roundTen, roundFifteen, roundTwenty);
-
-console.log(playerPoints, cpuPoints, resultText);
-
-console.log(btnRock, btnPaper, btnScissor);
-
 //functions
+
+//Computer Random number generator
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+const cpuInput = getRandomInt(3);
+console.log(cpuInput);
+  // Expected output: 0, 1 or 2
+
+
+// Rock button function
+btnRock.addEventListener("click", (event) =>{
+    const userInput = Number(btnRock.value);
+    const cpuInput = getRandomInt(3);
+
+    event.preventDefault();
+    
+    roundsSelection.style.display = "none";
+    roundsCounter.style.display = "block";
+
+    if (cpuInput === 2){
+        console.log("win");
+    } else if (cpuInput=== 1){
+        console.log('lose');
+    } else {
+        console.log("draw");
+    }
+
+})
+
+// Paper button function 
+btnPaper.addEventListener("click", (event) =>{
+    const userInput = Number(btnPaper.value);
+    const cpuInput = getRandomInt(3);
+
+    event.preventDefault();
+
+    if (cpuInput === 0){
+        console.log("win");
+    } else if (cpuInput=== 2){
+        console.log('lose');
+    } else {
+        console.log("draw");
+    }
+})
+
+//Scissor button function
+btnScissor.addEventListener("click", (event) =>{
+    const userInput = Number(btnScissor.value);
+    const cpuInput = getRandomInt(3);
+
+    event.preventDefault();
+
+    if (cpuInput === 1){
+        console.log("win");
+    } else if (cpuInput=== 0){
+        console.log('lose');
+    } else {
+        console.log("draw");
+    }
+})
