fee8d0163e4d3c22b51f62de1932f8e64d69f15e
karinlee <karinlee92@gmail.com>
2023-02-02 11:01:30 +0100

    added ids to html elements, added variables to javascript

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+//Variables
+
+// HTML Sections or text elements
+const rounds = document.querySelector("#rounds");
+const roundsCounter = document.querySelector("#roundsCounter");
+const roundOf = document.querySelector("#roundOf");
+
+//Rounds
+const roundFive = document.querySelector("#roundFive");
+const roundTen = document.querySelector("#roundTen");
+const roundFifteen = document.querySelector("#roundFifteen");
+const roundTwenty = document.querySelector("#roundTwenty");
+
+//Rock, Paper, Scissors buttons
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissor = document.querySelector("#scissor");
+
+//Output
+const playerPoints = document.querySelector("#playerPoints");
+const cpuPoints = document.querySelector("#cpuPoints");
+const resultText = document.querySelector("#resultText");
+
+console.log(rounds, roundsCounter, roundOf);
+
+console.log(roundFive, roundTen, roundFifteen, roundTwenty);
+
+console.log(playerPoints, cpuPoints, resultText);
+
+console.log(btnRock, btnPaper, btnScissor);
+
+//functions
