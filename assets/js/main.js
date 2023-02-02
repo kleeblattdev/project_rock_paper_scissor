836e41c395beb65897acfe169b8454165a2da1a6
karinlee <karinlee92@gmail.com>
2023-02-02 14:20:06 +0100

    added round counter function

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,8 @@ const roundFive = document.querySelector("#roundFive");
 const roundTen = document.querySelector("#roundTen");
 const roundFifteen = document.querySelector("#roundFifteen");
 const roundTwenty = document.querySelector("#roundTwenty");
+//
+let roundCounter = 0;
 
 //Rock, Paper, Scissors buttons
 const btnRock = document.querySelector("#rock");
@@ -21,30 +23,39 @@ const playerPoints = document.querySelector("#playerPoints");
 const cpuPoints = document.querySelector("#cpuPoints");
 const resultText = document.querySelector("#resultText");
 
+//reset Button
+const btnReset = document.querySelector("#reset");
+
+btnReset.addEventListener("click", () => {
+    window.location.reload();
+})
+
 //functions
 
 //Computer Random number generator
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-const cpuInput = getRandomInt(3);
-console.log(cpuInput);
-  // Expected output: 0, 1 or 2
 
+function computerPlay() {
+    let options = ["Rock", "Paper", "Scissor"];
+
+    return options[Math.floor(Math.random() * options.length)]; 
+}
 
 // Rock button function
 btnRock.addEventListener("click", (event) =>{
-    const userInput = Number(btnRock.value);
-    const cpuInput = getRandomInt(3);
+    const userInput = btnRock.value;
+    const cpuInput = computerPlay();
 
     event.preventDefault();
+    userRound(); // round counter 
     
     roundsSelection.style.display = "none";
     roundsCounter.style.display = "block";
 
-    if (cpuInput === 2){
+    if (cpuInput === "Scissor"){
+        //playerPoints++; // add point for player
         console.log("win");
-    } else if (cpuInput=== 1){
+    } else if (cpuInput === "Paper"){
+        //cpuPoints++; // add point for computer
         console.log('lose');
     } else {
         console.log("draw");
@@ -54,14 +65,14 @@ btnRock.addEventListener("click", (event) =>{
 
 // Paper button function 
 btnPaper.addEventListener("click", (event) =>{
-    const userInput = Number(btnPaper.value);
-    const cpuInput = getRandomInt(3);
+    const userInput = btnPaper.value;
+    const cpuInput = computerPlay();
 
     event.preventDefault();
 
-    if (cpuInput === 0){
+    if (cpuInput === "Rock"){
         console.log("win");
-    } else if (cpuInput=== 2){
+    } else if (cpuInput === "Scissor"){
         console.log('lose');
     } else {
         console.log("draw");
@@ -71,15 +82,38 @@ btnPaper.addEventListener("click", (event) =>{
 //Scissor button function
 btnScissor.addEventListener("click", (event) =>{
     const userInput = Number(btnScissor.value);
-    const cpuInput = getRandomInt(3);
+    const cpuInput = computerPlay();
 
     event.preventDefault();
 
-    if (cpuInput === 1){
+    if (cpuInput === "Paper"){
         console.log("win");
-    } else if (cpuInput=== 0){
+    } else if (cpuInput === "Scissor"){
         console.log('lose');
     } else {
         console.log("draw");
     }
 })
+
+//Roundcounter function
+
+const userRound = () => {
+    roundCounter++;
+    let counterOutput = roundCounter.toString();
+
+    if (roundFive.checked && roundCounter<=5){
+        roundOf.innerHTML = `${counterOutput} /5`;
+        return
+    }else if(roundTen.checked && roundCounter<=10){
+        roundOf.innerHTML = `${counterOutput} /10`;
+        return
+    }else if(roundFifteen.checked && roundCounter<=15){
+        roundOf.innerHTML = `${counterOutput} /15`;
+        return
+    }else if(roundTwenty.checked && roundCounter<=20){
+        roundOf.innerHTML = `${counterOutput} /20`;
+        return
+    }
+    else{
+    }
+}
